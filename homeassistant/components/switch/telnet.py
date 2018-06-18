21fc06232f3a8b6e99cc9eac981e02142c701d90
Benoit Louy <benoit.louy@fastmail.com>
2018-06-18 19:54:44 -0400

    Make telnet switch timeout configurable

--- homeassistant/components/switch/telnet.py
+++ homeassistant/components/switch/telnet.py
@@ -14,12 +14,14 @@ from homeassistant.components.switch import (
     ENTITY_ID_FORMAT, PLATFORM_SCHEMA, SwitchDevice)
 from homeassistant.const import (
     CONF_COMMAND_OFF, CONF_COMMAND_ON, CONF_COMMAND_STATE, CONF_NAME,
-    CONF_PORT, CONF_RESOURCE, CONF_SWITCHES, CONF_VALUE_TEMPLATE)
+    CONF_PORT, CONF_RESOURCE, CONF_SWITCHES, CONF_VALUE_TEMPLATE,
+    CONF_TIMEOUT)
 import homeassistant.helpers.config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
 
 DEFAULT_PORT = 23
+DEFAULT_TIMEOUT = 0.2
 
 SWITCH_SCHEMA = vol.Schema({
     vol.Required(CONF_COMMAND_OFF): cv.string,
@@ -29,6 +31,7 @@ SWITCH_SCHEMA = vol.Schema({
     vol.Optional(CONF_COMMAND_STATE): cv.string,
     vol.Optional(CONF_NAME): cv.string,
     vol.Optional(CONF_PORT, default=DEFAULT_PORT): cv.port,
+    vol.Optional(CONF_TIMEOUT, default=DEFAULT_TIMEOUT): cv.socket_timeout,
 })
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
@@ -55,6 +58,7 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
                 object_id,
                 device_config.get(CONF_RESOURCE),
                 device_config.get(CONF_PORT),
+                device.config.get(CONF_TIMEOUT),
                 device_config.get(CONF_NAME, object_id),
                 device_config.get(CONF_COMMAND_ON),
                 device_config.get(CONF_COMMAND_OFF),
@@ -73,13 +77,14 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
 class TelnetSwitch(SwitchDevice):
     """Representation of a switch that can be toggled using telnet commands."""
 
-    def __init__(self, hass, object_id, resource, port, friendly_name,
+    def __init__(self, hass, object_id, resource, port, timeout, friendly_name,
                  command_on, command_off, command_state, value_template):
         """Initialize the switch."""
         self._hass = hass
         self.entity_id = ENTITY_ID_FORMAT.format(object_id)
         self._resource = resource
         self._port = port
+        self._timeout = timeout
         self._name = friendly_name
         self._state = False
         self._command_on = command_on
@@ -88,16 +93,20 @@ class TelnetSwitch(SwitchDevice):
         self._value_template = value_template
 
     def _telnet_command(self, command):
+        telnet = None
         try:
             telnet = telnetlib.Telnet(self._resource, self._port)
             telnet.write(command.encode('ASCII') + b'\r')
-            response = telnet.read_until(b'\r', timeout=0.2)
+            response = telnet.read_until(b'\r', timeout=self._timeout)
             return response.decode('ASCII').strip()
         except IOError as error:
             _LOGGER.error(
                 'Command "%s" failed with exception: %s',
                 command, repr(error))
             return None
+        finally:
+            if telnet:
+                telnet.close()
 
     @property
     def name(self):
